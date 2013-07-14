446df7870cc46301658ccf955b73e9e6dc0e6d83
Benedikt Wegner <benedikt.wegner5@web.de>
2013-07-14 11:58:44 +0200

    fehlerfreier Programmstart

--- FizzBuzzGit/Program.cs
+++ FizzBuzzGit/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using NUnit.Framework;
 
 namespace FizzBuzzGit
 {
@@ -9,7 +11,40 @@ namespace FizzBuzzGit
     {
         static void Main(string[] args)
         {
+            var fizzBuzz = new FizzBuzz();
+
+            var result = fizzBuzz.Start();
+            Console.WriteLine(result);
+
+            var waitingClosing = Console.ReadKey();
             
+        }
+    }
+    public class FizzBuzz
+    {
+        public string Start()
+        {
+            var result = string.Empty;
+            return result;
+        }
+        
+    }
+
+    [TestFixture]
+    public class TestClass
+    {
+        private FizzBuzz _fizzBuzz;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fizzBuzz = new FizzBuzz();
+        }
+
+        [Test]
+        public void ShouldReturnNotNullWhenStart()
+        {
+            Assert.That(_fizzBuzz.Start(), Is.Not.Null);
         }
     }
 }
